7c03ee8d7ead65929af78f8d6f01174cb123219b
gabeFrancisco <gabegit2@gmail.com>
2023-04-29 00:38:47 -0300

    created TimelineItem component and timeline.js for data. started data insertion

---
+++ src/data/timeline.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    year: "2019",
+    title: "Python Básico por Gustavo Guanabara",
+    details:
+      "Meu primeiro contato com programação de fato. Apesar de não ser especializado em Python nos dias atuais, foi com ele que aprendi um pouco sobre lógica de programação. O ínico foi difícil, mas com muita persistência, as primeiras linhas de código funcionais apareceram e com isso, a paixão aumentava.",
+  },
+  {
+    year: "2020",
+    title: "Curso de C++ por Bruno Campos, livros e artigos",
+    details:
+      "Lembro quando tinha 10 anos, abri sem querer um codigo em C++(alguns arquivos C# também...) no computador de casa e fiquei maravilhado com aquele monte de código complicado, mesmo sem entender absolutamente nada. Depois de adulto, resolvi desvendar aquele enigma que encontrára quando criança e resolvi me desafiar a aprender a linguagem. Com C++ foi onde expandi ao máximo meus conhecimnetos sobre lógica de programação, algoritmos como Bubble Sort, Quick Sort, Merge Sort , etc. Aprendi muito sobre como Sistemas Operacionais funcionam, assim como compiladores, além de entender como é interagida na camada de baixo nível(assembly, memória, processador, arquiteturas). Sou grato por ter aprendido e compreendido a linaguagem, pois foi a porta de entrada para eu ter conhecido Arduino, sistemas embarcados e robótica em geral",
+  },
+  {
+    year: "2020",
+    title: "Curso de C# por Nélio Álves",
+    details:
+      "Aqui foi a primeira linguagem que me especializei. Apesar de linguagens de programação serem ferramentas, não posso negar que C# é a minha preferida, assim como o ambiente .NET. Foi minha porta de entrada para o universo Backend e aplicações comerciais. Através também do professor Nélio Alves, em conjunto com o C#m, aprendi modelagem de sistemas com UML. Tive meu primeiro contato com banco de dados relacionais(MySQL), assim como design Frontend, através de projetos MVC do frameowrk ASP.Net Core. Agora aqui tudo comecçou a fazer sentido pra mim. Toda a lógica aprendida comceçou a ser usada, comecei a implementar soluções e resolver problemas do mundo real e assim, nsaceu o projeto Dr. Chave",
+  },
+  {
+    year: "2020",
+    title: "Estudos de HTML e CSS",
+    details: "Grande partes dos meus conhecimentos iniciais aqui eu adquiri de diversas fontes: cursos online, documentações da W3C, Mozilla e principalmente pondo em pŕatica. Minhas primerias páginas estáticas foram criadas. Foi onde tive meu primeiro contato com Bootstrap e design de layouts"
+  },
+  {
+    year: "2021",
+    title: "Curso de banco de dados relacionais por Felipe Mafra",
+    details: "Percebi que para um bom programador Backend, é necessário um bom conhecimento em banco de dados. Com o excelente curso do professor Felipe Mafra, me aprofundei nos maiores e mais difundidos SGBDs como MySql, SQLServer e PostgreSQL. Obtive um conhecimento rico sobre linguagem SQL, manipulação de tables, relacionamentos e modelagem de dados em geral."
+  },
+  {
+    year: "2021",
+    title: "Curso avançado de ASP.Net Core MVC e WebApi",
+    details: "Como dito anteriormente, C# é a linaguagem que mais domino. Então nesse ano resolvi me especializar ainda mais nas tecnologias do ecosistema .NET. Com os cursos dos grandes professores, José Macoratti e Marcos Rosa aprendi conceitos avançados de Padrões de Projeto, Padrão MVC, Domain Driven Design, SOLID e arquitetura de software em geral, com foco em aplicações comerciais. Também aprendi e expandi meus conhecimentos sobre ORMs como Entity Framework e modelagem de dados. Com foco também em desenvolvimento de APIs, aprendi tudo sobre o procolo HTTP, bem como a interface RESTful."
+  }
+];
