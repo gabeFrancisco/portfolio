b8d41b809bed5abe7b342d0424de7428d9f2f641
gabeFrancisco <gabegit2@gmail.com>
2023-07-06 22:32:08 -0300

    Updates

--- assets/Title-f7540569.js
+++ assets/Title-f7540569.js
@@ -1,3 +1,3 @@
-import{j as r}from"./index-1ce37720.js";function n({children:e,id:t}){return r.jsx("h1",{id:t&&t,className:` rotate text-5xl
+import{j as r}from"./index-55a43ac1.js";function n({children:e,id:t}){return r.jsx("h1",{id:t&&t,className:` rotate text-5xl
         decoration-3 mb-20 text-stone-900 text-center dark:text-white bg-gradient-to-r from-blue-800 via-blue-700 to-green-600
         text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-blue-700 dark:via-blue-300 dark:to-green-400 text-gradient`,children:e})}export{n as T};
