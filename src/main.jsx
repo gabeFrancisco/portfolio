3c8efba88ca3fba8d6cf6eb07b840f5658fd559c
gabeFrancisco <gabegit2@gmail.com>
2023-07-07 21:57:06 -0300

    added react-snap for better SEO

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tailwind.css";
 import Loading from "./components/Loading.jsx";
+import { hydrate } from "react-dom";
+import { render } from "react-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+if(rootElement.hasChildNodes()){
+  hydrate( <React.StrictMode>
     <Suspense fallback={<Loading/>}>
       <App />
     </Suspense>
-  </React.StrictMode>
-);
+  </React.StrictMode>, rootElement)
+}
+else{
+  render(<React.StrictMode>
+    <Suspense fallback={<Loading/>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>, rootElement)
+}
+
