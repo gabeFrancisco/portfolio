47a9cbb1f70241dd7b3b2e9aacc0113ab372a7a3
gabeFrancisco <gabegit2@gmail.com>
2023-09-03 20:04:20 -0300

    added more data to portfolio items, fixed styles on card

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -10,20 +10,18 @@ export default function PortfolioItem({
   return (
     <div
       className="hover:cursor-pointer rounded-xl m-1 shadow-lg bg-gradient-to-br from-slate-100 to-slate-200
-      dark:bg-gradient-to-tr dark:from-zinc-900 dark:to-zinc-900 dark:bg-opacity-60 flex flex-col justify-between items-center align-middle"
+      dark:bg-gradient-to-tr dark:from-zinc-900 dark:to-zinc-900 dark:bg-opacity-60 flex flex-col justify-between"
       onClick={() => {
         window.open(github, "_blank").focus();
       }}
     >
       <div className="flex flex-col justify-between items-center">
         <img
-          style={{
-            borderRadius: "1.5rem",
-          }}
+         
           src={imgUrl}
           alt="portfolio"
           loading="lazy"
-          className="p-3 md:h-48 object-contain cursor-pointer overflow-hidden rounded-xl"
+          className="md:h-44 object-fill cursor-pointer overflow-hidden shadow-xl xl:h-64"
         />
         <div className="p-5">
           <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,12 +1,11 @@
 export default [
   {
     title: "Dr. Chave",
-    desc: "Primeiro projeto completo e funcional. Um gerenciador de chaves automotivas para chaveiros",
+    desc: "Primeiro projeto completo e funcional. Um gerenciador de chaves automotivas para chaveiros feito para Windows.",
     imgUrl: "/portfolio/assets/drkey.png",
     stack: [
       "devicon-csharp-plain colored",
       "devicon-windows8-original colored",
-      "devicon-flutter-plain colored",
       "devicon-mysql-plain colored",
     ],
     github: "https://github.com/gabeFrancisco/Dr-Key",
@@ -31,29 +30,29 @@ export default [
       "devicon-redux-original colored",
       "devicon-csharp-plain colored",
       "devicon-dotnetcore-plain colored",
-      "devicon-mysql-plain colored"
+      "devicon-mysql-plain colored",
     ],
     github: "https://github.com/gabeFrancisco/Redux-Storage",
   },
-  {
-    title: "Shop Management",
-    desc: "Aplicativo web para gerenciamento de uma loja. Ainda em desenvolvimento",
-    imgUrl:
-      "https://images.ctfassets.net/23aumh6u8s0i/5c2LVJHpVFgNW12LvVSnCg/a789ce83982ed4e63baff797495fc342/laravel-6-crud-app",
-    stack: [
-      "devicon-php-plain colored",
-      "devicon-laravel-plain colored",
-      "devicon-mysql-plain colored",
-    ],
-    github: "",
-  },
-  {
-    title: "Alicator",
-    desc: "Trata-se de um alimentador automático para gatos(mas serve para qualquer animal hahah), feito com Arduino. Ainda se encontra em desenvolvimento.",
-    imgUrl: "/portfolio/assets/alicator.png",
-    stack: ["devicon-cplusplus-plain colored", "devicon-arduino-plain colored"],
-    github: "",
-  },
+  // {
+  //   title: "Shop Management",
+  //   desc: "Aplicativo web para gerenciamento de uma loja. Ainda em desenvolvimento",
+  //   imgUrl:
+  //     "https://images.ctfassets.net/23aumh6u8s0i/5c2LVJHpVFgNW12LvVSnCg/a789ce83982ed4e63baff797495fc342/laravel-6-crud-app",
+  //   stack: [
+  //     "devicon-php-plain colored",
+  //     "devicon-laravel-plain colored",
+  //     "devicon-mysql-plain colored",
+  //   ],
+  //   github: "",
+  // },
+  // {
+  //   title: "Alicator",
+  //   desc: "Trata-se de um alimentador automático para gatos(mas serve para qualquer animal hahah), feito com Arduino. Ainda se encontra em desenvolvimento.",
+  //   imgUrl: "/portfolio/assets/alicator.png",
+  //   stack: ["devicon-cplusplus-plain colored", "devicon-arduino-plain colored"],
+  //   github: "",
+  // },
   {
     title: "LandUp Restaurant Suit",
     desc: "Meu maior projeto até o presente momento. Consiste em uma suíte de aplicativos para gerenciamento de restaurantes.",
@@ -72,14 +71,36 @@ export default [
     title: "Portfólio",
     desc: "Você esta acessando ele agora, porém, o ponto importante aqui é a inserção de modelos em 3D usando a biblioteca Three.JS e Fiber. Os logos das tecnologias foram feitos por mim através do Blender 3D.",
     imgUrl: "/portfolio/assets/portfolio.png",
-    stack: ["devicon-react-original colored", "devicon-tailwindcss-plain colored"],
+    stack: [
+      "devicon-react-original colored",
+      "devicon-tailwindcss-plain colored",
+    ],
     github: "https://github.com/gabeFrancisco/portfolio",
   },
   {
     title: "Hash Box",
     desc: "Uma pequena ferramenta para geração de hashes, GUIDs, string em Base64 e senhas Bcrypt. Foi desenvolvido com NextJS e hospedado na própria Vercel.",
     imgUrl: "/portfolio/assets/hashbox.png",
-    stack: ["devicon-react-original colored", "devicon-nextjs-line colored", "devicon-tailwindcss-plain colored", "devicon-nodejs-plain colored"],
+    stack: [
+      "devicon-react-original colored",
+      "devicon-nextjs-line colored",
+      "devicon-tailwindcss-plain colored",
+      "devicon-nodejs-plain colored",
+    ],
     github: "https://github.com/gabeFrancisco/hashBox",
+  },
+  {
+    title: "Doctor Key Web",
+    desc: "Segue o mesmo conceito do Dr. Chave, porém mais moderno! Seu frontend é totalmente feito com Next.JS e API em ASP.Net/C# com banco de dados PostgreSQL! Projeto ainda em desenvolvimento que contará com uma app em Flutter.",
+    imgUrl: "/portfolio/assets/doctorkeyweb.png",
+    stack: [
+      "devicon-react-original colored",
+      "devicon-nextjs-line colored",
+      "devicon-tailwindcss-plain colored",
+      "devicon-csharp-plain colored",
+      "devicon-dotnetcore-plain colored",
+      "devicon-postgresql-plain colored"
+    ],
+    github: "https://github.com/gabeFrancisco/DoctorKeyWeb",
   },
 ];
