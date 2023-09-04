bd09b1c63a7d46f8002ce3f237ebf9218e6dde62
gabeFrancisco <gabegit2@gmail.com>
2023-09-03 21:42:37 -0300

    Updates

---
+++ assets/Footer-137e0cd6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-688038c9.js";function o(){return e.jsx("footer",{className:"py-5 text-center",children:e.jsxs("p",{className:"text-sm mt-2 opacity-50",children:["© ",new Date().getFullYear()," - Portfólio desenvolvido por Gabriel Francisco"]})})}export{o as default};
