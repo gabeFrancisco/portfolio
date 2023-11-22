30190059933856b304bc8e7a6a5c10f298f797f9
gabeFrancisco <gabegit2@gmail.com>
2023-11-21 21:56:01 -0300

    added react-parallax-mouse

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,19 +2,20 @@ import React, { Suspense } from "react";
 import Me from "../../public/assets/me.jpg";
 import { TypeAnimation } from "react-type-animation";
 
+
 export default function Intro() {
   return (
     <>
-      <div id="home" className="flex flex-col items-center lg:flex-row lg:p-10 mb-10 justify-center">
+      <div id="home" className="flex flex-col items-center justify-center mb-10 lg:flex-row lg:p-10">
         <div
           className="mt-16 mx-10 md:mx-3 rounded-xl shadow-lg bg-gradient-to-r from-slate-100 to-slate-200 dark:bg-gradient-to-tr dark:from-zinc-950 dark:to-zinc-900
             flex flex-col items-center lg:flex-row lg:p-10 my-5
             justify-center; bg-opacity-20"
         >
-          <div className="p-10 lg:p-20 flex flex-col items-center hover:cursor-pointer">
+          <div className="flex flex-col items-center p-10 lg:p-20 hover:cursor-pointer">
             
             <img
-              className="w-64 lg:w-80 border-4 rounded-full border-slate-300 object-fill shadow-xl glass"
+              className="object-fill w-64 border-4 rounded-full shadow-xl lg:w-80 border-slate-300 glass"
               src={Me}
               alt=""
               loading="lazy"
@@ -24,17 +25,14 @@ export default function Intro() {
             />
           </div>
 
-          <div className="flex items-center justify-center flex-col text-center pt-20 pb-6 px-10">
+          <div className="flex flex-col items-center justify-center px-10 pt-20 pb-6 text-center">
             <h1
-              className="text-4xl md:text-7x1 mb-1  
-            md:mb-3 font-bold dark:text-white bg-gradient-to-r from-blue-800 via-blue-700 to-green-600
-            text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-blue-700 dark:via-blue-300 dark:to-green-400 text-gradient"
+              className="mb-1 text-4xl font-bold text-transparent md:text-7x1 md:mb-3 dark:text-white bg-gradient-to-r from-blue-800 via-blue-700 to-green-600 bg-clip-text dark:bg-gradient-to-r dark:from-blue-700 dark:via-blue-300 dark:to-green-400 text-gradient"
             >
               Olá, me chamo Gabriel Francisco
             </h1>
             <h3
-              className="text-base md:text-xl my-7 font-bold lg:text-2xl bg-gradient-to-r from-blue-800 via-blue-700 to-green-600
-            text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-blue-700 dark:via-blue-300 dark:to-green-400 text-gradient"
+              className="text-base font-bold text-transparent md:text-xl my-7 lg:text-2xl bg-gradient-to-r from-blue-800 via-blue-700 to-green-600 bg-clip-text dark:bg-gradient-to-r dark:from-blue-700 dark:via-blue-300 dark:to-green-400 text-gradient"
             >
               <TypeAnimation
                 className="text-2xl"
@@ -49,12 +47,13 @@ export default function Intro() {
                 repeat={Infinity}
               />
             </h3>
-            <p className="text-md max-w-xl m-3 font-bold text-slate-700 dark:text-slate-300">
+            
+            <p className="max-w-xl m-3 font-bold text-md text-slate-700 dark:text-slate-300">
               "Apaixonado por tecnologia, resolvendo problemas reais por meio de
               linhas de código..."
             </p>
             <br />
-            <p className="text-md max-w-xl mb-6 font-medium">
+            <p className="max-w-xl mb-6 font-medium text-md">
               Como um amante de tecnologia e curioso desde a infância, sempre
               procurei saber como as coisas funcionavam. Aos 10 anos
               tive meu primeiro contato com um computador e ali começou minha
@@ -63,7 +62,7 @@ export default function Intro() {
               jogos para computador("utilizando a infame engine The Games
               Factory").
             </p>
-            <p className="text-md max-w-xl mb-6 font-medium">
+            <p className="max-w-xl mb-6 font-medium text-md">
               Então, quando fiz 18, depois de longos anos sem contato com
               computação, reascendi a minha paixão quando tive meu primeiro
               contato com Python. Como de costume, as nossas primeiras linhas
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 import { motion } from "framer-motion";
+import {
+  MouseParallaxChild,
+  MouseParallaxContainer,
+} from "react-parallax-mouse";
 
 export default function PortfolioItem({
   title,
@@ -17,27 +21,31 @@ export default function PortfolioItem({
       transition={{
         type: "spring",
         stiffness: 260,
-        damping: 20
+        damping: 20,
       }}
-      className="hover:cursor-pointer rounded-xl m-1 shadow-lg bg-gradient-to-br from-slate-100 to-slate-200
-      dark:bg-gradient-to-tr dark:from-zinc-900 dark:to-zinc-900 dark:bg-opacity-60 flex flex-col justify-between"
+      className="flex flex-col justify-between m-1 shadow-lg hover:cursor-pointer rounded-xl bg-gradient-to-br from-slate-100 to-slate-200 dark:bg-gradient-to-tr dark:from-zinc-900 dark:to-zinc-900 dark:bg-opacity-60"
       onClick={() => {
         window.open(github, "_blank").focus();
       }}
     >
-      <div className="flex flex-col justify-between items-center">
-        <img
-          src={imgUrl}
-          alt="portfolio"
-          loading="lazy"
-          className="sm:h-32 md:h-40 lg:h-48 object-fill cursor-pointer overflow-hidden shadow-xl 2xl:h-72"
-        />
+      <div className="flex flex-col items-center justify-between">
+        {/* <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1}>
+          <MouseParallaxChild factorX={3} factorY={3}> */}
+            <img
+              src={imgUrl}
+              alt="portfolio"
+              loading="lazy"
+              className="object-fill overflow-hidden shadow-xl cursor-pointer sm:h-32 md:h-40 lg:h-48 2xl:h-72"
+            />
+          {/* </MouseParallaxChild>
+        </MouseParallaxContainer> */}
+
         <div className="p-5">
-          <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+          <h3 className="mb-2 text-lg font-semibold md:text-xl md:mb-3">
             {title}
           </h3>
 
-          <p className="text-base md:text-md mb-3 text-gray-700 dark:text-gray-400">
+          <p className="mb-3 text-base text-gray-700 md:text-md dark:text-gray-400">
             {description}
           </p>
         </div>
@@ -45,7 +53,7 @@ export default function PortfolioItem({
       <div className="w-full p-4">
         <p>
           {stack?.map((item, key) => (
-            // <span className="inline-block m-1 px-2 py-1 font-semibold border-2 border-stone-700 dark:border-stone-200 rounded-md">
+            // <span className="inline-block px-2 py-1 m-1 font-semibold border-2 rounded-md border-stone-700 dark:border-stone-200">
             //   {item}
             // </span>
             <i
