1ebc87b4906f4766de86e5105839e6362510224a
gabeFrancisco <gabegit2@gmail.com>
2023-12-17 22:54:33 -0300

    react router dom setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,38 @@ import "./styles/tailwind.css";
 import Loading from "./components/Loading.jsx";
 import { hydrate } from "react-dom";
 import { render } from "react-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Blog from "./routes/Blog.jsx";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: []
+  },
+  {
+    path: "/blog",
+    element: <Blog/>,
+  },
+]);
 
 const rootElement = document.getElementById("root");
-if(rootElement.hasChildNodes()){
-  hydrate( <React.StrictMode>
-    <Suspense fallback={<Loading/>}>
-      <App />
-    </Suspense>
-  </React.StrictMode>, rootElement)
-}
-else{
-  render(<React.StrictMode>
-    <Suspense fallback={<Loading/>}>
-      <App />
-    </Suspense>
-  </React.StrictMode>, rootElement)
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </React.StrictMode>,
+    rootElement
+  );
 }
